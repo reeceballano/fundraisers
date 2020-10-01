3a0d959f4ba83e46c34c83189e144af1b6b28722
Reece <trazhme@gmail.com>
2020-10-01 09:22:34 +0800

    update
    added fundraiser component

---
+++ src/components/Fundraiser.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="fundraiser-item shadow-md p-8">
+        item
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Fundraiser'
+}
+</script>
+
+<style>
+
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,31 @@
 <template>
     <div class="home">
         <section class="section">
-            <div class="container">
-                <h2>Home</h2>
+            <div class="container md:px-24">
+                <div class="py-24">
+                    <div class="grid md:grid-cols-3 gap-10">
+                        <div class="md:col-span-1 shadow-md p-8">
+                            <h3>Options</h3>
+                        </div>
+
+                        <div class="md:col-span-2">
+                            <FundraiserItem></FundraiserItem>
+                        </div>
+                    </div>
+
+                </div>
             </div>
         </section>
     </div>
 </template>
 
 <script>
+import FundraiserItem from '@/components/Fundraiser';
 
 export default {
-  name: 'Home',
-  components: {
-  }
+    name: 'Home',
+    components: {
+        FundraiserItem,
+    }
 }
 </script>
