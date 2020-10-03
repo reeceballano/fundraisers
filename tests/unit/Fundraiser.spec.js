f2f6b02222ea8f2e09626b2296344ba0426e7260
Reece <trazhme@gmail.com>
2020-10-03 08:09:57 +0800

    test
    added a test for computed properties

--- tests/unit/Fundraiser.spec.js
+++ tests/unit/Fundraiser.spec.js
@@ -1,4 +1,4 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount } from '@vue/test-utils'
 import Fundraiser from '@/components/Fundraiser.vue'
 
 test('test fundraiser props', () => {
@@ -13,4 +13,21 @@ test('test fundraiser props', () => {
     })
 
     expect(wrapper.props().fundraiser).toBe(data);
+})
+
+describe('Fundraiser', () => {
+    it('returns a comma separated total', () => {
+        const data = {
+            
+            Total: "12345.11"
+        }
+    
+        const wrapper = shallowMount(Fundraiser, {
+            propsData: {
+                fundraiser: data
+            }
+        })
+        
+        expect(wrapper.vm.numberWithCommas).toBe("12,345.11")
+    })
 })
