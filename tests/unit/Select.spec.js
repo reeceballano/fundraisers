0e6375b66a806d7e521007e1d51be12a6c52844b
RDB <rdbwebdeveloper@gmail.com>
2020-10-03 16:51:06 +0800

    update

--- tests/unit/Select.spec.js
+++ tests/unit/Select.spec.js
@@ -1,18 +1,23 @@
-import { mount } from '@vue/test-utils'
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import Select from '@/components/Select.vue'
+import Vuex from 'vuex'
+
+const localVue = createLocalVue()
+localVue.use(Vuex)
 
 describe('Select', () => {
-    const data = {
+    const props = {
         name: 'selectItemCount',
         data: ['S', 'T']
     }
 
     it('props should contain a "name" "data"', () => {
         const wrapper = mount(Select, {
-            propsData: data
+            propsData: props
         })
 
         expect(wrapper.props().name).toBe('selectItemCount');
         expect(wrapper.props().data).toStrictEqual(['S', 'T']);
     })
+
 })
