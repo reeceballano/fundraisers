9640fb765bb1cbfcb25b98da35d8fc255975d37b
Reece <trazhme@gmail.com>
2020-10-03 12:16:12 +0800

    refactor

--- tests/unit/Fundraiser.spec.js
+++ tests/unit/Fundraiser.spec.js
@@ -1,27 +1,14 @@
 import { mount, shallowMount } from '@vue/test-utils'
 import Fundraiser from '@/components/Fundraiser.vue'
 
-test('test fundraiser props', () => {
+describe('Fundraiser', () => {
+
     const data = {
-        Id: 123
+        Id: 123,
+        Total: '12345.11'
     }
 
-    const wrapper = mount(Fundraiser, {   
-        propsData: {
-            fundraiser: data
-        }
-    })
-
-    expect(wrapper.props().fundraiser).toBe(data);
-})
-
-describe('Fundraiser', () => {
-    it('returns a comma separated total', () => {
-        const data = {
-            
-            Total: "12345.11"
-        }
-    
+    it('should return a comma separated total', () => {
         const wrapper = shallowMount(Fundraiser, {
             propsData: {
                 fundraiser: data
@@ -29,5 +16,15 @@ describe('Fundraiser', () => {
         })
         
         expect(wrapper.vm.numberWithCommas).toBe("12,345.11")
+    })
+
+    it('props should contain an object type', () => {
+        const wrapper = mount(Fundraiser, {   
+            propsData: {
+                fundraiser: data
+            }
+        })
+    
+        expect(wrapper.props().fundraiser).toBe(data);
     })
 })
